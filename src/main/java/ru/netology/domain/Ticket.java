52bc5079012f687812ab7caaff9e7f6b2dccf92e
Petr Filatov <pmfilatov@gmail.com>
2020-11-04 15:32:55 +0300

    add domain/manager/repo

---
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -0,0 +1,62 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket implements Comparable<Ticket> {
+    private int id;
+    private int price;
+    private String from;
+    private  String to;
+    private  int time;
+
+
+        @Override
+        public int compareTo(Ticket o) {
+            return price - o.price;
+        }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
+
---
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -0,0 +1,32 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Ticket;
+import ru.netology.repository.TicketRepository;
+
+import java.util.Arrays;
+
+public class TicketManager {
+    private TicketRepository repository;
+
+    public TicketManager(TicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public Ticket[] findTickets(String from, String to) {
+        Ticket[] tickets = new Ticket[0];
+        Ticket[] ticketsAll = repository.getAll();
+        for (Ticket ticket : ticketsAll) {
+            if (ticket.getFrom().equalsIgnoreCase(from) && ticket.getTo().equalsIgnoreCase(to)) {
+                int length = tickets.length + 1;
+                Ticket[] tmp = new Ticket[length];
+                System.arraycopy(tickets, 0, tmp, 0, tickets.length);
+                int lastIndex = tmp.length - 1;
+                tmp[lastIndex] = ticket;
+                tickets = tmp;
+            }
+        }
+        Arrays.sort(tickets);
+        return tickets;
+    }
+
+}
